810b0182b234f4ca8d30ba110df6c8dc71e4950b
Ali Oğuzhan YÜCE <91226446+aliousyucex@users.noreply.github.com>
2022-10-02 15:37:58 +0300

    Sendgrid & Score

--- src/server/routes/router.js
+++ src/server/routes/router.js
@@ -4,17 +4,44 @@ const express = require('express');
 const res = require('express/lib/response');
 const Router = express.Router();
 const mysqlCon = require('../db/connection');
+const sendEmail = require('@sendgrid/mail')
+
+sendEmail.setApiKey(process.env.SENDGRID_API_KEY)
 Router.use(express.json())
+
 
 Router.get('/score', (req, res) => {
     mysqlCon.query('SELECT * FROM score', (err, rows) => {
+        if (!err) {
+            const sortData = [];
+            for(var scoreText in rows) {
+                rows.sort(function(a, b) {
+                    return a.scoreInt - b.scoreInt;
+                });
+            }
+            res.send(rows);
+        }
+        else {
+            console.log(err);
+        }
+    });
+});
+
+Router.post('/score', (req, res) => {
+    mysqlCon.query(`
+    INSERT INTO score 
+        (userName, scoreText, scoreInt) 
+    VALUES  
+        ('${req.body.userName}','${req.body.scoreText}',${req.body.scoreInt});
+    `,
+    (err, rows) => {
         if (!err) {
             res.send(rows);
         }
         else {
             console.log(err);
         }
-    })
+    });
 });
 
 Router.get('/subscribe', (req, res) => {
@@ -41,6 +68,18 @@ Router.post('/subscribe', (req, res) => {
                 `,
                 (err, rows, fields) => {
                     if (!err) {
+
+                        const maildata = {
+                            to: req.body.email,
+                            from: 'sponsorship@elephantyachtclub.com',
+                            templateId: 'd-9e67dc44ceb248f588a45f416d1e3e39',
+                        }
+
+                        sendEmail.send(maildata).then(() => {
+                            console.log('Email sent')
+                        }).catch((error) => {
+                         console.error(error)
+  })
                         res.send({msg:'Sucsessfuly added adress!'});
                     }
                     else {
